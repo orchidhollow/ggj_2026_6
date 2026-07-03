73ed1dd5ce468cc4da5f1546499c4a8c4dc9643e
orchidhollow <895002501@qq.com>
2026-07-03 20:29:42 +0800

    删除多余代码

--- Assets/Scripts/Config/IConfigOverview.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-    public interface IConfigOverview<T1, T2> where T2 : ConfigBase
-    {
-        string FolderName { get; }
-        int NextBaseConfigId { get; }
-        List<T1> AllKeys { get; }
-        List<T2> AllValues { get; }
-    }
